cf150d299f53116219c435a3d1916da3a72ecfff
Miren Tanna <mirrent@gmail.com>
2015-03-23 18:07:33 -0500

    updated gamma and delta compressions

--- IR2.java
+++ IR2.java
@@ -1,3 +1,5 @@
+import org.jsoup.Jsoup;
+
 import java.io.*;
 import java.util.*;
 
@@ -8,8 +10,11 @@ import java.util.*;
 * */
 public class IR2 {
     static TreeMap<String, String> stopwordsMap= new TreeMap<String, String>();
-    static TreeMap<String, TreeMap<Integer, Integer>> index= new TreeMap<String, TreeMap<Integer, Integer>>();
-    static File uncompressedIndex;
+    static TreeMap<String, TreeMap<Integer, Integer>> indexVersion1= new TreeMap<String, TreeMap<Integer, Integer>>();
+    static TreeMap<String, LinkedHashMap<Short, Short>> compressedIndexVersion1= new TreeMap<String, LinkedHashMap<Short, Short>>();
+    static TreeMap<String, TreeMap<Integer, Integer>> indexVersion2= new TreeMap<String, TreeMap<Integer, Integer>>();
+    static TreeMap<String, LinkedHashMap<Short, Short>> compressedIndexVersion2= new TreeMap<String, LinkedHashMap<Short, Short>>();
+    static File uncompressedIndexFile, compressedIndexFile;
 
     static int numberOfFiles=0;
     public static void main(String[] args) {
@@ -37,19 +42,59 @@ public class IR2 {
         int[][] documents = new int[numberOfFiles + 1][2];
         TreeMap<String, String> stopwords= getStopwords("stopwords");
 
+        System.out.println("Version 1");
+        System.out.println("Creating index");
+        long startTimeVersion1= System.currentTimeMillis();
         for(File files:listOfFiles){
             int docID=Integer.parseInt(files.getName().substring(9));
 
+ //           HashMap<String, Integer> tokensInFile= getTokens(files);
+ //           HashMap<String, Integer> stemmedTokens= getStemmedTokens(tokensInFile);
             HashMap<String, Integer> lemmaHashMap= new Lemmatizer().lemmatize(files);
             HashMap<String, Integer> lemmaNormalizedHashMap= removeStopwords(lemmaHashMap, stopwords);
          //working   System.out.println(docId + " " + lemmaNormalizedHashMap.size());
             //TODO: Version 1: Dictionary compression--> blocked ______ posting file--> gamma
             //TODO: Version 2: Dictionary compression--> front coding ______ posting file--> delta
 
-            createIndex(docID, lemmaNormalizedHashMap);
+            createIndex(docID, lemmaNormalizedHashMap, indexVersion1);
 
         }
-        writeUncompressedIndex("./", "Index_Version1.uncompressed");
+        double timeTakenVersion1= (System.currentTimeMillis() - startTimeVersion1)/1000;
+        System.out.println("Writing uncompressed index..");
+        writeUncompressedIndex("./", "Index_Version1.uncompressed", indexVersion1);
+        System.out.println("Done..\nCompressing index..");
+        compressIndex(indexVersion1, compressedIndexVersion1, "gamma");
+        System.out.println("Done..\nWriting compressed index..");
+        writeCompressedIndex("./", "Index_Version1.compressed", compressedIndexVersion1);
+        System.out.println("Done..");
+        System.out.println("Time taken by version 1: " + timeTakenVersion1);
+
+        System.out.println("*******************************************");
+        System.out.println("Version 2");
+        System.out.println("Creating index");
+        long startTimeVersion2= System.currentTimeMillis();
+        for(File files:listOfFiles){
+            int docID=Integer.parseInt(files.getName().substring(9));
+
+            HashMap<String, Integer> tokensInFile= getTokens(files);
+   //         System.out.println(files.getName());
+            HashMap<String, Integer> stemmedTokens= getStemmedTokens(tokensInFile);
+            HashMap<String, Integer> stemmedNormalizedHashMap= removeStopwords(stemmedTokens, stopwords);
+            //working   System.out.println(docId + " " + lemmaNormalizedHashMap.size());
+            //TODO: Version 1: Dictionary compression--> blocked ______ posting file--> gamma
+            //TODO: Version 2: Dictionary compression--> front coding ______ posting file--> delta
+
+            createIndex(docID, stemmedNormalizedHashMap, indexVersion2);
+        }
+        double timeTakenVersion2= (System.currentTimeMillis() - startTimeVersion2)/1000;
+        System.out.println("Writing uncompressed index..");
+        writeUncompressedIndex("./", "Index_Version2.uncompressed", indexVersion2);
+        System.out.println("Done..\nCompressing index..");
+        compressIndex(indexVersion2, compressedIndexVersion2, "delta");
+        System.out.println("Done..\nWriting compressed index..");
+        writeCompressedIndex("./", "Index_Version2.compressed", compressedIndexVersion2);
+        System.out.println("Done..");
+        System.out.println("Time taken by version 2: " + timeTakenVersion2);
 
     } /*
     TODO: document frequency --> dF, need file of document information?
@@ -64,6 +109,91 @@ public class IR2 {
         read.close();
         return stopwordsMap;
     }
+    public static HashMap<String, Integer> getStemmedTokens(HashMap<String, Integer> tokensInFile){
+        HashMap<String, Integer> stemmedTokens= new HashMap<String, Integer>();
+
+        Stemmer stemmer= new Stemmer();
+        for(String token: tokensInFile.keySet()){
+            stemmer.add(token.toCharArray(), token.length());
+            stemmer.stem();
+            String stemmedToken= stemmer.toString();
+            if(stemmedTokens.get(stemmedToken)==null){
+    /*TODO: is this right?*/stemmedTokens.put(stemmedToken, 1);
+            }
+            else{
+                stemmedTokens.put(stemmedToken, stemmedTokens.get(stemmedToken)+1);
+            }
+        }
+        return stemmedTokens;
+    }
+    public static HashMap<String, Integer> getTokens(File file){
+        HashMap<String, Integer> tokensInFile= new HashMap<String, Integer>();
+        ArrayList<String> normalizedTokens= getNormalizedTokens(file);
+        for(String nTokens: normalizedTokens){
+            ArrayList<String> noPuncTokens= removePunctuations(nTokens);
+            if(noPuncTokens!=null){
+                for(String noPuncToken: noPuncTokens){
+                    if(tokensInFile.get(noPuncToken)==null){
+                        tokensInFile.put(noPuncToken, 1);
+                    }
+                    else{
+                        tokensInFile.put(noPuncToken, tokensInFile.get(noPuncToken)+1);
+                    }
+                }
+            }
+        }
+        return tokensInFile;
+    }
+    public static ArrayList<String> removePunctuations(String tokens){
+        ArrayList<String> noPuncTokens= new ArrayList<String>();
+        ArrayList<String> noPuncTokensCopy= new ArrayList<String>();
+        if(tokens.contains("-")){
+            String noHyphens[]= tokens.split("-");
+            for(String noH: noHyphens){
+                noPuncTokens.add(noH.toLowerCase());
+            }
+        }
+        else
+            noPuncTokens.add(tokens.toLowerCase());
+
+        for(String token: noPuncTokens){
+            if(token.contains(".") && token.length()>1){
+                noPuncTokensCopy.add(token.replaceAll("\\.", ""));
+            }
+            else if(!token.contains("."))
+                noPuncTokensCopy.add(token.toLowerCase());
+        }
+        noPuncTokens.clear();
+
+        for(String token: noPuncTokensCopy){
+            if(token.endsWith("'s")){
+                noPuncTokens.add(token.replace("'s", "").trim());
+            }
+            else if(token.contains("'") && token.length()>1){
+                noPuncTokens.add(token.replace("'", ""));
+            }
+            else if(!token.contains("'"))
+                noPuncTokens.add(token.toLowerCase());
+        }
+        return noPuncTokens;
+    }
+    public static ArrayList<String> getNormalizedTokens(File file){
+        ArrayList<String> tokens= new ArrayList<String>();
+        try {
+            String fileContent= Jsoup.parse(file, "UTF-8").text();
+            String normalizedTokens= fileContent.replaceAll("[^a-zA-Z0-9'.]", " ").replaceAll("[\\s+]", " ").trim();
+            String normalizedTokensArray[]= normalizedTokens.split(" ");
+
+            for(String t: normalizedTokensArray){
+                if(t.length()>0){
+                    tokens.add(t.trim());
+                }
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+            }
+        return tokens;
+    }
     public static HashMap<String, Integer> removeStopwords(HashMap<String, Integer> lemmaHashMap, TreeMap<String, String> stopwords) throws FileNotFoundException {
         Iterator<Map.Entry<String, Integer>> lemmaIterator= lemmaHashMap.entrySet().iterator();
         HashMap<String, Integer> lemmaNormalizedHashMap= new HashMap<String, Integer>();
@@ -77,15 +207,15 @@ public class IR2 {
     }
 
     /********************************index creation methods*********************************/
-    public static void createIndex(int docID, HashMap<String, Integer> lemmaNormalizedHashMap){
-        Iterator<Map.Entry<String, Integer>> iterator= lemmaNormalizedHashMap.entrySet().iterator();
+    public static void createIndex(int docID, HashMap<String, Integer> normalizedHashMap, TreeMap<String, TreeMap<Integer, Integer>> index){
+        Iterator<Map.Entry<String, Integer>> iterator= normalizedHashMap.entrySet().iterator();
         while(iterator.hasNext()){
             Map.Entry<String, Integer> entry=iterator.next();
             // entry.getkey, docid, entry.getvalue(entrygetkey)
-            insertInIndex(entry.getKey(), docID, entry.getValue());
+            insertInIndex(entry.getKey(), docID, entry.getValue(), index);
         }
     }
-    public static void insertInIndex(String term, int docID, int termFrequency){
+    public static void insertInIndex(String term, int docID, int termFrequency, TreeMap<String, TreeMap<Integer, Integer>> index){
         TreeMap<Integer, Integer> currentPostingList= index.get(term);
         if(currentPostingList!=null){
             currentPostingList.put(docID, termFrequency);
@@ -97,11 +227,84 @@ public class IR2 {
             index.put(term, newPostingList);
         }
     }
-    public static void writeUncompressedIndex(String indexDirectoryPath, String indexName) throws IOException {
-        uncompressedIndex= new File(indexDirectoryPath +"/"+ indexName);
-        ObjectOutputStream objectOutputStream= new ObjectOutputStream(new FileOutputStream(uncompressedIndex));
+    public static void writeUncompressedIndex(String indexDirectoryPath, String indexName, TreeMap<String, TreeMap<Integer, Integer>> index) throws IOException {
+        uncompressedIndexFile= new File(indexDirectoryPath +"/"+ indexName);
+        ObjectOutputStream objectOutputStream= new ObjectOutputStream(new FileOutputStream(uncompressedIndexFile));
         objectOutputStream.writeObject(index);
         objectOutputStream.flush();
         objectOutputStream.close();
+    }
+    public static void writeCompressedIndex(String indexDirectoryPath, String indexName, TreeMap<String, LinkedHashMap<Short, Short>> compressedIndex) throws IOException {
+        compressedIndexFile= new File(indexDirectoryPath +"/"+ indexName);
+        ObjectOutputStream objectOutputStream= new ObjectOutputStream(new FileOutputStream(compressedIndexFile));
+        objectOutputStream.writeObject(compressedIndex);
+        objectOutputStream.flush();
+        objectOutputStream.close();
+    }
+    public static void compressIndex(TreeMap<String, TreeMap<Integer, Integer>> index, TreeMap<String, LinkedHashMap<Short, Short>> compressedIndex, String encodingType){
+        Set<String> terms= index.keySet();
+        for(String term: terms){
+            TreeMap<Integer, Integer> postingList= index.get(term);
+            LinkedHashMap<Short, Short> postingListWithGaps= new LinkedHashMap<Short, Short>();
+            int originalGap=0;
+            for(Map.Entry<Integer, Integer> entry: postingList.entrySet()){
+                originalGap= entry.getKey()-originalGap;
+                if(encodingType.equals("gamma"))
+                    postingListWithGaps.put(gammaEncoding(originalGap), gammaEncoding(entry.getValue()));
+                else if(encodingType.equals("delta"))
+                    postingListWithGaps.put(deltaEncoding(originalGap), deltaEncoding(entry.getValue()));
+
+                originalGap= entry.getKey();
+            }
+            compressedIndex.put(term, postingListWithGaps);
+        }
+    }
+    public static short gammaEncoding(int valueToEncode){   //valueToEncode= 5
+        int offsetUnaryLength=0;
+        int unary=0;
+        int valueToEncodeCopy=valueToEncode;
+
+        while(valueToEncodeCopy!=1){
+            valueToEncodeCopy/=2;
+            offsetUnaryLength++;        //offsetUnaryLength= 2
+        }
+        int offset=1 << offsetUnaryLength;  //offset= 1<<2= 100
+        offset-=1;                          //offset= 100-1= 99
+        offset=offset & valueToEncode;  //offset= 99&5= 1
+        int offsetLengthCopy= offsetUnaryLength;        //offsetLengthCopy= 2
+        while(offsetUnaryLength!=0){
+            unary= unary << 1;
+            unary= unary | 1;
+            offsetUnaryLength--;
+        }                                   //unary= 11
+        unary= unary << 1;                  //unary= 11<<1= 110
+        unary= unary << offsetLengthCopy;   //unary= 110<<2= 11000
+        offset= offset | unary; // offset= 1|11000= 11001
+
+        return (short)offset;
+    }
+    public static short deltaEncoding(int valueToEncode){   //valueToEncode=5
+        int offsetUnaryLength=1;
+        int valueToEncodeCopy=valueToEncode;
+
+        while(valueToEncodeCopy>1){
+            valueToEncodeCopy/=2;
+            offsetUnaryLength++;        //offsetUnaryLength= 3
+        }
+        int code= gammaEncoding(offsetUnaryLength);     //code=101
+        int offsetUnaryLengthCopy=0;
+        int valueToEncodeCopy2=valueToEncode;       //valueToEncodeCopy2=5
+        while (valueToEncodeCopy2!=1){
+            valueToEncodeCopy2/=2;
+            offsetUnaryLengthCopy++;            //offsetUnaryLengthCopy=2
+        }
+        int offset=1 << offsetUnaryLengthCopy;  //offset= 1<<2= 100
+        offset-=1;                          //offset= 100-1= 99
+        offset=offset & valueToEncode;  //offset= 99&5= 1
+        code= code << offsetUnaryLengthCopy;   //code= 101<<2= 10100
+        code= offset | code; // offset= 1|10100= 10101
+
+        return (short)code;
+
     }
 }
